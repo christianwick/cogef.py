98e1f26238bd123eadd5247140a73641ab315800
Christian <christian.wick@fau.de>
2021-08-10 06:56:56 +0200

    added basic version with setuptools_scm

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cogef.py",
+    author="Christian Wick",
+    author_email="christian.wick@fau.de",
+    description="Cogef.py - COnstraint Geometries simulate external force",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    use_scm_version = True,
+    setup_requires = ["setuptools_scm"]
+)
