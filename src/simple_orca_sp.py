628e7bce0c8018063b82dfc352a1c1108dcda8d1
Christian <christian.wick@fau.de>
2021-09-03 16:08:01 +0200

    updatad s simple_orca_sp.py with logging

--- src/simple_orca_sp.py
+++ src/simple_orca_sp.py
@@ -10,13 +10,15 @@ import csv
 import json
 import argparse
 import subprocess
+import logging
 
 from cogef.constants import *
 from cogef import modstruct
 from cogef import gaussian
 from cogef import molecule
+from cogef import cogef_logging
 
-
+from cogef._version import __version__ 
 
 
 
@@ -27,7 +29,23 @@ if __name__ == "__main__":
     parser.add_argument("-use_last_gbw", help="use last gbw file as input", action="store_true", default=False)
     parser.add_argument("-guess_orbs", help="use in guess orbs 'guess_orbs.gbw'", type=str, default="guess_orbs.gbw")
 
+    group_logging = parser.add_argument_group("logging")
+    group_logging.add_argument("-log_level", help="set the log level", choices=["DEBUG","INFO"], default="INFO")
+    group_logging.add_argument("-stream_level", help="set the streaming level", choices=["DEBUG","INFO"], default="INFO")
+
     args = parser.parse_args()
+
+
+    # START LOGGING HERE
+    # this way no logs are created for e.g. -h or --version 
+    cogef_logging.logging_init(log_file="simple_orca_sp.log", log_level = args.log_level, stream_level = args.stream_level)
+    logger = logging.getLogger("simple_orca_sp")
+    logger.info("Starting orca single point calculations .... " )
+    logger.info("Version: {}".format(__version__))
+    logger.info("Command line Arguments: ")
+    for arg,value in (vars(args)).items():
+        logger.info("    -{:10s} : {}".format(arg,value))
+
 
     xyztrj = molecule.Molecule.read_xyz_trj(args.xyztrj)
     xyztrj.reverse()
@@ -50,4 +68,7 @@ if __name__ == "__main__":
             orca_inp = orca_inp.replace("LASTGBW",gbw_guess)
         with open(filename+".inp", "w") as of:
             of.write(orca_inp)
-        subprocess.run(["runorca_4_2",filename + ".inp"], check=True)
+        runorca = subprocess.run(["runorca_4_2",filename + ".inp"], check=True, capture_output=True, text=True)
+
+    # STOP LOGGING HERE
+    logger.info("Finished gaussian single point calculations. " )
